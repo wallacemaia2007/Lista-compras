64ea2a4f98a6227a3903121c02b26317a2d7c2ab
Wallace Maia <wallacemaia2007@gmail.com>
2025-10-07 10:33:22 -0300

    Feat: Implementing TypeScripty logic

--- src/app/lista-compras/lista-compras.ts
+++ src/app/lista-compras/lista-compras.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ItemLista } from './itemLista';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-compras.html',
-  styleUrl: './lista-compras.scss'
+  styleUrl: './lista-compras.scss',
 })
 export class ListaCompras {
+  lista: ItemLista[] = [];
+  item: string = '';
 
+  adicionarItem() {
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+
+    this.lista.push(itemLista);
+    this.item = '';
+  }
+
+  riscarItem(itemLista: ItemLista) {
+    itemLista.comprado = !itemLista.comprado;
+  }
+
+  limparLita() {
+    this.lista = [];
+  }
 }
