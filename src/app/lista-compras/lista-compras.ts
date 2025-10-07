65cb3df206dc02a46adfcabc34802ae6adc48c8b
Wallace Maia <wallacemaia2007@gmail.com>
2025-10-06 23:34:37 -0300

    Inicial Setup: HTML and SCSS

---
+++ src/app/lista-compras/lista-compras.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-compras',
+  imports: [],
+  templateUrl: './lista-compras.html',
+  styleUrl: './lista-compras.scss'
+})
+export class ListaCompras {
+
+}
